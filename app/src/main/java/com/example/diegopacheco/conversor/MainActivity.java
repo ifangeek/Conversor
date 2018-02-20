3e227dbd63cd256a16676d4482661de620d55245
Diego Josue <dpachecorosas96@gmail.com>
2018-02-20 00:32:18 -0500

    Conversor no muestra los spinners seleccionados

--- app/src/main/java/com/example/diegopacheco/conversor/MainActivity.java
+++ app/src/main/java/com/example/diegopacheco/conversor/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.diegopacheco.conversor;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -17,60 +20,82 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    Spinner desde;
-    HashMap<String,String> monedaList;
-    List<Currencies> listaCurrencies;
+    Spinner desde,hacia;
+    HashMap<String, String> monedaList;
+    List<String> listaCurrencies = null;
+    public static final String BASE_URL = "http://apilayer.net/api/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        desde = (Spinner)findViewById(R.id.spnDesde);
-        monedaList= new HashMap<String,String>();
+        desde = (Spinner) findViewById(R.id.spnDesde);
+        hacia = (Spinner)findViewById(R.id.spnHacia);
+        monedaList = new HashMap<String, String>();
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://apilayer.net")
+                .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         MonedaService service = retrofit.create(MonedaService.class);
 
         Call<Moneda> call = service.getMoneda();
-        Response<Moneda> response ;
-        final Currencies currencies = null;
+        Response<Moneda> response;
+        listaCurrencies = new ArrayList<>();
 
         try {
-           call.enqueue(new Callback<Moneda>() {
+            call.enqueue(new Callback<Moneda>() {
 
-               @Override
-               public void onResponse(Call<Moneda> call, Response<Moneda> response) {
-                   Log.d("RESPUESTA", response.toString());
+                @Override
+                public void onResponse(Call<Moneda> call, Response<Moneda> response) {
+                    Log.d("RESPUESTA", response.toString());
 
-                       Moneda moneda = response.body();
-                           monedaList =  moneda.getMonedas();
-                           Log.d("RESPUESTA", monedaList.toString());
+                    Moneda moneda = response.body();
+                    monedaList = moneda.getMonedas();
+                    Log.d("RESPUESTA", monedaList.toString());
 
-                           /* for(String key : monedaList.keySet()){
-                                ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                                        android.R.layout.simple_spinner_dropdown_item,key);
-                               // currencies.setCodigo(key.toString());
-                                Log.d("RESPUESTA",key);
-                            }*/
-                  // Log.d("CURRENCIES",currencies.getCodigo());
-
+                    for (String key : monedaList.keySet()) {
+                        Log.d("RESPUESTA", key);
+                        monedaList.get(key);
+                        listaCurrencies.add(monedaList.get(key));
+                        Log.d("RESPUESTA", monedaList.get(key) + "");
+                    }
 
 
+                     Log.d("CURRENCIES",listaCurrencies.toString());
+                }
 
-               }
-
-               @Override
-               public void onFailure(Call<Moneda> call, Throwable t) {
-                   Log.d("RESPUESTA", t.getMessage());
-               }
-           });
+                @Override
+                public void onFailure(Call<Moneda> call, Throwable t) {
+                    Log.d("RESPUESTA", t.getMessage());
+                }
+            });
         } catch (Exception e) {
             e.printStackTrace();
         }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_dropdown_item,listaCurrencies);
+        desde.setAdapter(adapter);
+
+        desde.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("posicion", String.valueOf(desde.getItemAtPosition(i)));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this,android.R.layout.simple_spinner_dropdown_item,listaCurrencies);
+        hacia.setAdapter(adapter1);
+
+
+
 
     }
+
+
 }
--- app/src/main/java/com/example/diegopacheco/conversor/MonedaService.java
+++ app/src/main/java/com/example/diegopacheco/conversor/MonedaService.java
@@ -1,15 +1,12 @@
 package com.example.diegopacheco.conversor;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface MonedaService {
 
 
-    @GET("/api/list?access_key=bd025437f1f1f4b0fd9299526c12e8d5&format=1")
+    @GET("list?access_key=bd025437f1f1f4b0fd9299526c12e8d5&format=1")
     Call<Moneda> getMoneda();
 
 }
